b84d0b741038146703e5ad7c97959a64026972ce
DanaDos <79756184+DanaDos@users.noreply.github.com>
2021-05-25 01:52:01 -0500

    Update Class1.java
    remote second commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -6,12 +6,15 @@ public class Class1 {
 	
 	public static void main(String[] args) {
 		
-		
 		//Hello everyone
 		//Welcome to Git Sessions
 		//Thank you for joining
 		//Here is a new commit
+		
 		//here is an issue on local main branch
+		
+		//Here is remote change
+		//Here is another remote action
 	}
 
 }
